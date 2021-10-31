632609f87dbc3727ad4e3b3b83f69afd197fdd20
Denise Hung <denisehung87@gmail.com>
2021-11-01 00:06:48 +0100

    fix: remove input error message placeholder

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -8,13 +8,13 @@ function SignIn(props) {
       <div className="popup__input-wrapper">
         <label className="popup__input-label" for="email-input">Email</label>
         <input type="email" className="popup__input" id="email-input" autoComplete="off" placeholder="Enter email" name="email" required />
-        <p id="email-input-error" className="popup__error">blabla</p>
+        <p id="email-input-error" className="popup__error"></p>
       </div>
 
       <div className="popup__input-wrapper">
         <label className="popup__input-label" for="password-input">Password</label>
         <input type="password" className="popup__input" id="password-input" autoComplete="off" placeholder="Enter password" name="password" required />
-        <p id="password-input-error" className="popup__error">blabla</p>
+        <p id="password-input-error" className="popup__error"></p>
       </div>
 
       <button className="popup__submit-button" type="submit" aria-label="Sign in">Sign in</button>
