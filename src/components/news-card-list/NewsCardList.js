d254ce5ed220661c485b5882bdd9c90bc4060870
cjmaret <cjmaret@gmail.com>
2021-11-20 13:34:13 +0700

    feat: parameter changes

--- src/components/news-card-list/NewsCardList.js
+++ src/components/news-card-list/NewsCardList.js
@@ -41,30 +41,12 @@ function NewsCardList({
     setNext(next + 3);
   }
 
-  return (
-    <section
-      className={`news-card-list ${
-        onSavedArticlesPage && 'news-card-list_saved-articles'
-      }`}
-    >
-      <div className="news-card-list__container">
-        {!onSavedArticlesPage && (
-          <h3 className="news-card-list__title">Search results</h3>
-        )}
-        <ul
-          className={`news-card-list__card-grid ${
-            onSavedArticlesPage && 'news-card-list__card-grid_saved-articles'
-          }`}
-        >
-          {displayedCards.map((newscard, index) => (
-            <li className="news-card-list__card" key={index}>
-            
   return onSavedArticlesPage ? (
-    <section className='news-card-list news-card-list_saved-articles'>
-      <div className='news-card-list__container'>
-        <ul className='news-card-list__card-grid news-card-list__card-grid_saved-articles'>
+    <section className="news-card-list news-card-list_saved-articles">
+      <div className="news-card-list__container">
+        <ul className="news-card-list__card-grid news-card-list__card-grid_saved-articles">
           {displayedCards?.map((newscard) => (
-            <li className='news-card-list__card' key={newscard._id}>
+            <li className="news-card-list__card" key={newscard._id}>
               <NewsCard
                 data={newscard}
                 onSavedArticlesPage={onSavedArticlesPage}
@@ -77,13 +59,12 @@ function NewsCardList({
       </div>
     </section>
   ) : (
-    <section className='news-card-list'>
-      <div className='news-card-list__container'>
-        <h3 className='news-card-list__title'>Search results</h3>
-        <ul className='news-card-list__card-grid'>
+    <section className="news-card-list">
+      <div className="news-card-list__container">
+        <h3 className="news-card-list__title">Search results</h3>
+        <ul className="news-card-list__card-grid">
           {displayedCards?.map((newscard, index) => (
-            <li className='news-card-list__card' key={index}>
-
+            <li className="news-card-list__card" key={index}>
               <NewsCard
                 data={newscard}
                 onSavedArticlesPage={onSavedArticlesPage}
@@ -104,7 +85,7 @@ function NewsCardList({
           </button>
         )}
         <button
-          className='news-card-list__show-more-button'
+          className="news-card-list__show-more-button"
           onClick={handleShowMoreCards}
         >
           Show more
--- src/components/news-card/NewsCard.js
+++ src/components/news-card/NewsCard.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './NewsCard.css';
 
-function NewsCard({ data, onSavedArticlesPage, loggedIn, onSaveArticleClick, savedArticlesData }) {
+function NewsCard({
+  data,
+  onSavedArticlesPage,
+  loggedIn,
+  onSaveArticleClick,
+  savedArticlesData,
+}) {
   const [isSaved, setIsSaved] = useState(false);
 
   useEffect(() => {
-   if (savedArticlesData.find((obj) => obj.title === data.title)) {
-     setIsSaved(true);
-   }
+    if (savedArticlesData.find((obj) => obj.title === data.title)) {
+      setIsSaved(true);
+    }
   }, [data.title, savedArticlesData]);
 
   function handleSave() {
@@ -39,93 +45,49 @@ function NewsCard({ data, onSavedArticlesPage, loggedIn, onSaveArticleClick, sav
     return convertedDate;
   }
 
-  return (
-    <div className="news-card">
-      {!onSavedArticlesPage && (
-        <>
-          <button
-            className={`news-card__button news-card__button_save ${
-              loggedIn && isSaved ? 'news-card__button_save_active' : ''
-            }`}
-            onClick={handleSave}
-          ></button>
-          {!loggedIn && (
-            <div className="news-card__tag news-card__tag_type_tooltip">
-              Sign in to save articles
-            </div>
-          )}
-        </>
-      )}
-
-      {onSavedArticlesPage && (
-        <>
-          <button className="news-card__button news-card__button_delete"></button>
-          <div className="news-card__tag news-card__tag_type_tooltip">
-            Remove from saved
-          </div>
-          <div className="news-card__tag news-card__tag_type_keyword">
-            Keyword
-          </div>
-        </>
-      )}
-
-      <a href={data.url} className="news-card__link">
-        <img
-          src={data.urlToImage}
-          alt={data.title}
-          className="news-card__image"
-        />
-        <div className="news-card__wrapper">
-          <p className="news-card__date">{convertDate()}</p>
-          <h2 className="news-card__title">{data.title}</h2>
-          <p className="news-card__description">{data.description}</p>
-          <p className="news-card__source">{data.source.name}</p>
-        </div>
-      </a>
-
   return onSavedArticlesPage ? (
-    <div className='news-card'>
-      <img src={data.image} alt={data.title} className='news-card__image' />
-      <button className='news-card__button news-card__button_delete'></button>
-      <div className='news-card__tag news-card__tag_type_tooltip'>
+    <div className="news-card">
+      <img src={data.image} alt={data.title} className="news-card__image" />
+      <button className="news-card__button news-card__button_delete"></button>
+      <div className="news-card__tag news-card__tag_type_tooltip">
         Remove from saved
       </div>
-      <div className='news-card__tag news-card__tag_type_keyword'>
+      <div className="news-card__tag news-card__tag_type_keyword">
         {data.keyword}
       </div>
-      <div className='news-card__details'>
-        <p className='news-card__date'>{convertDate()}</p>
-        <h2 className='news-card__title'>{data.title}</h2>
-        <p className='news-card__description'>{data.text}</p>
-        <p className='news-card__source'>{data.source}</p>
+      <div className="news-card__details">
+        <p className="news-card__date">{convertDate()}</p>
+        <h2 className="news-card__title">{data.title}</h2>
+        <p className="news-card__description">{data.text}</p>
+        <p className="news-card__source">{data.source}</p>
       </div>
     </div>
   ) : (
-    <div className='news-card'>
+    <div className="news-card">
       <img
         src={data.urlToImage}
         alt={data.title}
-        className='news-card__image'
+        className="news-card__image"
       />
       <button
         className={`news-card__button news-card__button_save ${
           loggedIn && isSaved ? 'news-card__button_save_active' : ''
         }`}
         onClick={() => {
-          onSaveArticleClick(data)
+          onSaveArticleClick(data);
           handleSave();
         }}
       ></button>
       {!loggedIn && (
-        <div className='news-card__tag news-card__tag_type_tooltip'>
+        <div className="news-card__tag news-card__tag_type_tooltip">
           Sign in to save articles
         </div>
       )}
-      <div className='news-card__details'>
-        <p className='news-card__date'>{convertDate()}</p>
-        <h2 className='news-card__title'>{data.title}</h2>
-        <p className='news-card__description'>{data.description}</p>
-        <p className='news-card__source'>{data.source?.name}</p>
+      <div className="news-card__details">
+        <p className="news-card__date">{convertDate()}</p>
+        <h2 className="news-card__title">{data.title}</h2>
+        <p className="news-card__description">{data.description}</p>
+        <p className="news-card__source">{data.source?.name}</p>
       </div>
     </div>
   );
