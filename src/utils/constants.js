5fd55491230d9d46ae43bb64a89aeb3730c835c7
Denise Hung <denisehung87@gmail.com>
2021-11-21 21:58:30 +0100

    feat: create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+const API_KEY = 'b02901b7a4fa48e8aec2fe30bb52e6a6';
+const PROXY_URL = 'https://nomoreparties.co/news/v2/everything';
+const NUM_CARDS = 3;
+const NUM_ARTICLES = 100;
+const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.aloha.students.nomoreparties.site'
+    : 'http://localhost:3000';
+
+module.exports = {
+  API_KEY,
+  BASE_URL,
+  PROXY_URL,
+  NUM_CARDS,
+  NUM_ARTICLES,
+};
