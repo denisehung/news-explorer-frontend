1b7684d1ddd66ea13cac478ee9784ae8e50d9548
Denise Hung <denisehung87@gmail.com>
2021-12-30 14:07:09 +0100

    fix: update backend domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,9 @@ const API_KEY = 'b02901b7a4fa48e8aec2fe30bb52e6a6';
 const PROXY_URL = 'https://nomoreparties.co/news/v2/everything';
 const NUM_CARDS = 3;
 const NUM_ARTICLES = 100;
-const BASE_URL = 'https://api.aloha.students.nomoreparties.site';
-  /*process.env.NODE_ENV === 'production'
-    ? 'https://api.aloha.students.nomoreparties.site'
-    : 'http://localhost:3000';*/
+const BASE_URL = process.env.NODE_ENV === 'production'
+    ? 'https://api.aloha.students.nomoreparties.sbs'
+    : 'http://localhost:3000';
 
 module.exports = {
   API_KEY,
